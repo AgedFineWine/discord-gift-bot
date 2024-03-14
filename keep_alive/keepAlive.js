44b91abe7bc77d306dd6c7830e7cd5d49f5fd2cc
Evan He <eh4359@rit.edu>
2024-03-14 13:59:54 -0400

    changed keepAlive.js to not depend on Express.js

--- keep_alive/keepAlive.js
+++ keep_alive/keepAlive.js
@@ -1,14 +1,6 @@
-const express = require('express');
-const server = express();
+let http = require('http');
 
-server.all('/', (req, res)=>{
-  res.send('Bot is running!');
-});
-
-function keepAlive(){
-  server.listen(3000, () => {
-    console.log('Server is ready.');
-  });
-}
-
-module.exports = keepAlive;
+http.createServer((req, res) => {
+  res.write("I'm alive");
+  res.end();
+}).listen(8080);
