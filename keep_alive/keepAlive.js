68b390cf07b2e2e4440e65f2f3673dae53055a36
Evan He <eh4359@rit.edu>
2024-03-14 14:10:44 -0400

    turned main content into function in keepAlive.js to be exported

--- keep_alive/keepAlive.js
+++ keep_alive/keepAlive.js
@@ -1,6 +1,10 @@
 let http = require('http');
 
-http.createServer((req, res) => {
-  res.write("I'm alive");
-  res.end();
-}).listen(8080);
+function keepAlive() {
+  http.createServer((req, res) => {
+    res.write("I'm alive");
+    res.end();
+  }).listen(8080);
+}
+
+module.exports = keepAlive;
